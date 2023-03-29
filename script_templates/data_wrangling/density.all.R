2b7d9c216179cc02ab01e0d5589b5d491f90b415
Marney Pratt <mcpratt@smith.edu>
2023-03-29 09:12:58 -0400

    renamed to density.all

--- script_templates/data_wrangling/density.all.R
+++ script_templates/data_wrangling/density.all.R
@@ -8,13 +8,9 @@ macro.density <- macros %>%
   #join taxa info
   left_join(., master.taxa) %>%
   
-  
   # Sum for each sampleID and the different taxa 
   # density of ALL macroinvertebrates
   # change group_by function to remove or add grouping variables as needed 
-  # if you want a specific group of organisms, add that column name into 
-  # the list of grouping variables (family, organism_aggr, FFG, etc)
-  # then filter for the organism or group you want
   group_by(date, sampleID, season, location, year, benthicArea) %>% 
   dplyr::summarise (density = sum(invDens, na.rm = TRUE)) 
 
